2f92f8b34051f5ff7cefb488a1fc85878768e4f6
Mattia Pompita <mattiapompita@gmail.com>
2021-10-25 12:50:46 +0200

    upload image

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,9 +1,12 @@
-import style from './SkillCard.module.css';
+import style from "./SkillCard.module.css";
 
 const SkillCard = (props) => {
   return (
     <li className={style.skillCard}>
-      
+      <div className={style.container}>
+        <img className={style.icon} src={props.img}></img>
+        <h3 className={style.text}>{props.name}</h3>
+      </div>
     </li>
   );
 };
